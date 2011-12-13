cd9080e08b9cb9ddb3dbdb9bad3aa8b1aaf68e7b
Gleb <newmen@blackbox.(none)>
2011-12-14 01:03:02 +0400

    1.9.2 checker

--- lib/playlist_string.rb
+++ lib/playlist_string.rb
@@ -2,6 +2,11 @@ require 'active_support/all'
 
 class PlaylistString < String
   def ==(other)
-    self.mb_chars.downcase == other.mb_chars.downcase
+    begin
+      self.mb_chars.downcase == other.mb_chars.downcase
+    rescue SystemStackError
+      STDERR << "Ваша версия Ruby не позволяет обрабатывать разный регистр русских букв. Обновите Ruby до версии не ниже 1.9.2\n"
+      exit!
+    end
   end
 end
